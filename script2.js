4e36842f40803fb46facc66b2f4cb78b8a4265fd
SUSP8129 <superspeedbirds@outlook.jp>
2024-10-14 14:24:16 +0900

    update script2.js

--- script2.js
+++ script2.js
@@ -1,7 +1,9 @@
-document.getElementById('my-button').addEventListener('click', function() {
+document.getElementById('my-button').addEventListener('click', function move(url) {
     if (confirm('Do you want to open an external site?')) {
       console.log('OK');
+      location.href = url;
     } else {
       console.log('Cancel');
+      return;
     }
   })
